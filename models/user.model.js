c0e898a848de3bf69f71a5e3f4584684dc449f3c
Max Mayer <mxhmayer@gmail.com>
2022-01-06 18:24:29 +0000

    add user rout and edit user models

--- models/user.model.js
+++ models/user.model.js
@@ -5,15 +5,12 @@ const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   username: { type: String, required: true },
-  birth: { type:Date },
+  birthday: { type: Date },
   phone: { type: Number },
   name: { type: String, required: true },
   profileImg: { type: String, default: "https://i.imgur.com/yWHfhiG.png" },
-  followers: [{ type: String }],
-  following: [{ type: String }],
-  savedPost: [{ type: Schema.Types.ObjectId, ref: "Post" }],
 });
 
-// acessar com celular e ou email 
+// acessar com celular e ou email
 
 module.exports = model("User", userSchema);
--- routes/user.routes.inst.js
+++ routes/user.routes.inst.js
@@ -1,3 +1,72 @@
-const express = require('Express');
+const express = require("Express");
 const router = express.Router();
-const User = require('./) 
+const User = require("../models/user.model");
+
+// rota de todos os usuarios
+router.get("/api/users", async (req, res, next) => {
+  try {
+    // ! verificar o que se trata payload
+    const currentUser = req.payload;
+    console.log("currentUser :>> ", currentUser);
+
+    const user = await User.findById(currentUser._id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    // ! verificar o uso next
+    // ! next(error);
+    res.status(500).json(error);
+  }
+});
+
+/// rota para editar o usuario
+router.put("api/users", async (req, res, next) => {
+  try {
+    const currentUser = req.payload;
+    const { email, password, username, birthday, phone, name, profileImg } =
+      req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      currentUser._id,
+      {
+        email,
+        password,
+        username,
+        birthday,
+        phone,
+        name,
+        profileImg,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete("api/users/:userId", async (req, res, next) => {
+  try {
+    // ! estudar descontruction
+    const { currentUserId } = req.params;
+    await User.findIdAndDelete(currentUserId);
+
+    res.status(200).json(currentUserId);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("api/users/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const oneUser = await User.findById(userId);
+
+    res.status(200).json(oneUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
